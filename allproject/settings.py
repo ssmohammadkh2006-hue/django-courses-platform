957e9fcc592b146fe7d0af38242f203755d80ce6
Mohammad_ALnimrawi <ssmohammad.kh2006@gmail.com>
2026-03-19 01:04:04 +0300

    prepare project for render deploymxxxt

--- allproject/settings.py
+++ allproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j7mx03$ofb#6vy3h_kk0%1(&5po&i459+6l&-fpej(x(6#^b*#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
